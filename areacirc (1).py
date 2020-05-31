a6dc88dbc68fb4503c90a3691e88a9695e670650
Tejal10 <66191795+Tejal10@users.noreply.github.com>
2020-05-31 10:45:00 +0530

    areaofcircle
    pythonfile

---
+++ areacirc (1).py
@@ -0,0 +1,52 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Input radius of circle1.1\n",
+      "The area of the circle is 3.7994000000000008\n"
+     ]
+    }
+   ],
+   "source": [
+    "r=input(\"Input radius of circle\")\n",
+    "area=3.14*float(r)*float(r)\n",
+    "print(\"The area of the circle is \" + str(area))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
